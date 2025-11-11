2264d4ffbaa5aba8d0738f65bfb4a1602a3fe25c
ZHamburglar <kollinbrandenburg@gmail.com>
2025-11-11 15:39:27 -0600

    updating to output envs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -17,6 +17,13 @@ const start = async () => {
     throw new Error("MYSQL_DATABASE must be defined");
   }
 
+  if (process.env.MYSQL_DATABASE && process.env.MYSQL_PASSWORD && process.env.MYSQL_USER && process.env.MYSQL_HOST) {
+    console.log("Connecting to MySQL with the following config:");
+    console.log(`Host: ${process.env.MYSQL_HOST}`);
+    console.log(`User: ${process.env.MYSQL_USER}`);
+    console.log(`Database: ${process.env.MYSQL_DATABASE}`);
+  }
+
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!!");
   });
