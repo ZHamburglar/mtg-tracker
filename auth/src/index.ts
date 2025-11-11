1b00548b32700f306d31be791e9c3bee875735b4
ZHamburglar <kollinbrandenburg@gmail.com>
2025-11-10 22:44:17 -0600

    updating build to arm64

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 
 const start = async () => {
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!!!!!");
+    console.log("Listening on port 3000!!!!!!");
   });
 };
 
