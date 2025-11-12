51d95f14b906bf54bd24382a37d1dcf3b6920fdb
ZHamburglar <kollinbrandenburg@gmail.com>
2025-11-12 01:23:57 -0600

    different base requests for envs

---
+++ mtg-tracker-client/components/header.js
@@ -0,0 +1,48 @@
+import { NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuIndicator,
+  NavigationMenuViewport } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
+
+
+const Header = ({ currentUser }) => {
+  return (
+    <nav className="border-b">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <NavigationMenuLink href="/">Home</NavigationMenuLink>
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <NavigationMenuLink href="/search">Search</NavigationMenuLink>
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <NavigationMenuLink href="/">Home</NavigationMenuLink>
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button>Dark Mode</Button>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
