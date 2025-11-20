583f0ddcda29173d4a6ce29af89aecbe2ce0dd34
ZHamburglar <kollinbrandenburg@gmail.com>
2025-11-20 17:44:43 -0600

    adding migrations to collection

--- collection/src/migrations/002_add_available_column_up.sql
+++ collection/src/migrations/002_add_available_column_up.sql
@@ -4,12 +4,6 @@
 -- This tracks how many cards are available for listing/trading
 -- available = quantity - (cards currently listed/sold)
 
+-- Step 1: Add the column with default value
 ALTER TABLE user_card_collection
   ADD COLUMN available INT NOT NULL DEFAULT 0 AFTER quantity;
-
--- Set available = quantity for all existing records
-UPDATE user_card_collection SET available = quantity;
-
--- Add check constraint to ensure available doesn't exceed quantity
-ALTER TABLE user_card_collection
-  ADD CONSTRAINT check_available_quantity CHECK (available >= 0 AND available <= quantity);
