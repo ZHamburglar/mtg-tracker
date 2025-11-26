bcf50e4063075275da0057a24f576ddbbd1c8927
ZHamburglar <kollinbrandenburg@gmail.com>
2025-11-26 01:00:57 -0600

    updating mysql connection

--- collection/src/config/mysql.ts
+++ collection/src/config/mysql.ts
@@ -13,6 +13,9 @@ export async function createMysqlPoolWithRetry({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000,
+    connectTimeout: 60000,
   });
 
   while (retries > 0) {
