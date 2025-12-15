efb96e9eaed4aaf3e83d4f784d87a0f0a1a0b622
ZHamburglar <kollinbrandenburg@gmail.com>
2025-12-15 14:35:53 -0600

    updating notification

--- client/app/layout.js
+++ client/app/layout.js
@@ -16,19 +16,6 @@ export const metadata = {
   },
 };
 
-async function getCurrentUser() {
-  try {
-    const client = buildClient();
-    console.log('Fetching current user from server-side...');
-    const { data } = await client.get('/api/users/currentuser');
-    console.log('Current user data:', data);
-    return data.currentUser;
-  } catch (error) {
-    console.error('Error fetching current user:', error.message);
-    return null;
-  }
-}
-
 export default async function RootLayout({ children }) {
   // const currentUser = await getCurrentUser();
   
