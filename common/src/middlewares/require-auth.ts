c13762e46fd7a41fcc5c5151257a3a0a238ccc76
ZHamburglar <kollinbrandenburg@gmail.com>
2025-12-18 17:08:18 -0600

    updating common

--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -1,18 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../errors/custom-error';
-
-export class NotAuthorizedError extends CustomError {
-  statusCode = 401;
-
-  constructor() {
-    super('Not authorized');
-    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
-  }
-
-  serializeErrors() {
-    return [{ message: 'Not authorized' }];
-  }
-}
+import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAuth = (
   req: Request,
