c5d0bbe9eca6dedfe512fc0f75d25e35cb1126c5
ZHamburglar <kollinbrandenburg@gmail.com>
2025-12-18 17:27:00 -0600

    causing to run common

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,7 @@
+//Functions
 export * from './functions/runMigrations';
 
+// Errors
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
@@ -7,11 +9,13 @@ export * from './errors/request-validation-error';
 export * from './errors/not-found-error';
 export * from './errors/not-authorized-error';
 
+// Middlewares
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 export * from './middlewares/require-admin';
 
+// Logs
 export * from './logs/service-log';
 
