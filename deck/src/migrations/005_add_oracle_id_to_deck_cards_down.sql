3e7270ea883f2375d2b46fb48e01b10a5970eef9
ZHamburglar <kollinbrandenburg@gmail.com>
2025-12-29 13:24:41 -0600

    updating the add the card oracle ID

---
+++ deck/src/migrations/005_add_oracle_id_to_deck_cards_down.sql
@@ -0,0 +1,10 @@
+```sql
+-- Migration: Remove oracle_id column from deck_cards
+-- Service: deck
+-- Description: Reverts addition of oracle_id column
+
+ALTER TABLE deck_cards
+  DROP INDEX IF EXISTS idx_deck_cards_oracle_id,
+  DROP COLUMN oracle_id;
+
+```
